4ee7505d6a028f32015eeb677e758b0612210779
PabloDavidCruzCruz <201222@ids.upchiapas/edu.mx>
2022-06-29 19:32:02 -0500

    MAN-11 Creacion del metodo post para agregar datos al json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,50 @@
-const { Router, json } = require("express");
+const {Router} = require('express');
 const router = Router();
+const fs = require('fs');
+const { v4 } = require('uuid');
 
-router.get('/test', (req, res) =>{
-    const data = {
-        "name": "Artorias",
-        "Poder": "Mago"
+const json_enemigos = fs.readFileSync('src/enemigos.json','utf-8');
+let enemigos = JSON.parse(json_enemigos)
+
+router.get('/',(req,res)=>{
+    res.render('index.ejs',{
+        enemigos
+    })
+});
+
+//RenderizarFormulario
+router.get('/new-entry',(req,res)=>{
+    res.render('new-entry');
+});
+
+router.post('/new-entry',(req,res)=>{
+    const {title,tipo,debilidad,dificultad,image,descripcion} = req.body;
+    if(!title || !tipo || !debilidad || !dificultad || !image || !descripcion){
+        res.status(400).send('Campos Incompletos');
+        return;
+    }
+    
+    let newEnemigo = {
+        id: v4(),
+        title,
+        tipo,
+        debilidad,
+        dificultad,
+        image,
+        descripcion
     };
-    res.json(data);
+    
+    enemigos.push(newEnemigo);
+    const json_enemigos = JSON.stringify(enemigos)
+    fs.writeFileSync('src/enemigos.json',json_enemigos,'utf-8');
+    res.redirect('/');
+});
+
+router.get('/delete/:id',(req,res) => {
+    enemigos = enemigos.filter(enemigo => enemigo.id !=req.params.id);
+    const json_enemigos = JSON.stringify(enemigos)
+    fs.writeFileSync('src/enemigos.json',json_enemigos,'utf-8');
+    res.redirect('/');
 });
 
 module.exports = router;
